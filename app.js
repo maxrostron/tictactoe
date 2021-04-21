fe82e2a4ded5bc56878d18fd610a50e533ba3206
maxrostron <maxrostron@hotmail.co.uk>
2021-04-21 17:41:33 +0100

    Establish base HTML and CSS

---
+++ app.js
@@ -0,0 +1,18 @@
+// Make module for gameboard
+
+let gameBoard = (function () {
+
+
+
+    return {
+
+
+    };
+})();
+
+//NB need to load a board of arrays and push them as divs with the right class
+
+
+
+// Make factory for players?
+    // maybe have a property for difficulty
